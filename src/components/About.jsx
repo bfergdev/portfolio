5fc047f76f8434abf112e82eebd2647d7843c3a4
bfergdev <bferg.dev@gmail.com>
2026-02-23 01:16:34 -0500

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion'
-import { Swords, Cog, Handshake, DraftingCompass } from 'lucide-react'
+import { Swords, Cog, DraftingCompass, Handshake } from 'lucide-react'
 
 const About = () => {
   const features = [
